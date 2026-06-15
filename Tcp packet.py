4e73626228d3fd90df2938b17a615627c2a06d41
Thanisha S <Thanisha0527@gmail.com>
2026-06-15 13:40:47 +0530

    Update Tcp packet.py

--- Tcp packet.py
+++ Tcp packet.py
@@ -1,14 +1,14 @@
 from scapy.all import *
 
-# TCP SYN Packet
+
 packet = IP(dst="8.8.8.8") / TCP(
     sport=12345,
     dport=80,
     flags="S"
 )
 
-# Packet details
+
 packet.show()
 
-# Send packet
+
 send(packet)
